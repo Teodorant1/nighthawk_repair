c206f525871c26f71eeec9dca045117e6d94c839
Dusan <dusanbojanic1@gmail.com>
2023-11-15 13:48:23 +0100

    2nd commit

--- app/api/registration/route.tsx
+++ app/api/registration/route.tsx
@@ -1,5 +1,46 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+import bcrypt from "bcrypt";
+
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(5),
+});
+
+// interface registerPayload {
+//   email: string;
+//   password: string;
+// }
 
 export async function POST(req: NextRequest) {
-  const body = await req.json;
+  console.log("paloki");
+  const body = await req.json();
+  const validation = schema.safeParse(body);
+  if (!validation) {
+    return NextResponse.json(validation, {
+      status: 400,
+    });
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+    },
+  });
+
+  if (user) {
+    return NextResponse.json({ error: "user already exists" }, { status: 400 });
+  }
+
+  const hashedpassword = await bcrypt.hash(body.password, 10);
+
+  const newuser = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: hashedpassword,
+    },
+  });
+
+  return NextResponse.json({ email: newuser.email });
 }
